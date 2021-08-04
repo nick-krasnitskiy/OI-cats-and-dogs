011143dfe27ac137d297d5d0ee978da572186891
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2021-08-04 12:35:36 +0300

    Add user long tapped test pins with temperature to map.

--- DogsList/DogsList/Modules/Weather/WeatherMapViewController.swift
+++ DogsList/DogsList/Modules/Weather/WeatherMapViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 import MapKit
 
 class WeatherMapViewController: UIViewController {
-
+    
+    @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addPinTapAction(mapView: mapView, target:self, action: #selector(addAnnotation))
     }
-
-
+    
+    func addPinTapAction(mapView: MKMapView, target: AnyObject, action: Selector, tapDuration: Double = 0.1) {
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: target, action: action)
+        longPressRecognizer.minimumPressDuration = tapDuration
+        mapView.addGestureRecognizer(longPressRecognizer)
+    }
+    
+    @objc func addAnnotation(gestureRecognizer:UIGestureRecognizer) {
+        
+        let location = getTappedLocation(mapView: self.mapView, gestureRecognizer: gestureRecognizer)
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        annotation.title = "Temperature = 20 C"
+        //annotation.subtitle = "City name"
+        
+        mapView.addAnnotation(annotation)
+    }
+    
+    func getTappedLocation(mapView: MKMapView, gestureRecognizer: UIGestureRecognizer) -> CLLocationCoordinate2D {
+         
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        return mapView.convert(touchPoint, toCoordinateFrom: mapView)
+    }
 }
