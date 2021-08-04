4e893eb2e1c6ac6df7948090b056c2822f8e35d1
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2021-08-04 09:08:21 +0300

    Create UI for the main screen.

---
+++ DogsList/DogsList/Modules/Weather/WeatherMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherMapViewController.swift
+//  DogsList
+//
+//  Created by Nick Krasnitskiy on 04.08.2021.
+//
+
+import UIKit
+import MapKit
+
+class WeatherMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
