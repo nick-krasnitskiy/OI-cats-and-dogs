fc4b6dba808dd16fae88198956f563ac96a10d30
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2021-08-09 14:34:17 +0300

    Add a custom pin image. Implement a callout display after the image loading. Add an info button for open detail VC.

--- DogsList/DogsList/Modules/Weather/WeatherMapViewController.swift
+++ DogsList/DogsList/Modules/Weather/WeatherMapViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 import MapKit
 
+class CustomPin: NSObject, MKAnnotation {
+    var title: String?
+    var subtitle: String?
+    var coordinate: CLLocationCoordinate2D
+    
+    init(pinTitle: String, pinSubTitle: String, pinLocation: CLLocationCoordinate2D) {
+        self.title = pinTitle
+        self.subtitle = pinSubTitle
+        self.coordinate = pinLocation
+    }
+}
+
 class WeatherMapViewController: UIViewController {
     
     @IBOutlet private weak var mapView: MKMapView!
@@ -15,11 +27,6 @@ class WeatherMapViewController: UIViewController {
     
     var weatherManager = WeatherManager()
     
-    private var name: String?
-    private var temperature: String?
-    private var latitude: Double?
-    private var longitude: Double?
-
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
@@ -34,57 +41,38 @@ class WeatherMapViewController: UIViewController {
         let longPressRecognizer = UILongPressGestureRecognizer(target: target, action: action)
         longPressRecognizer.minimumPressDuration = tapDuration
         mapView.addGestureRecognizer(longPressRecognizer)
+        
     }
     
     @objc func addTapAnnotation(gestureRecognizer:UIGestureRecognizer) {
         
-        let annotation = MKPointAnnotation()
         let location = getTappedLocation(mapView: self.mapView, gestureRecognizer: gestureRecognizer)
-        annotation.coordinate = location
         weatherManager.fetchWeather(latitude: location.latitude, longitude: location.longitude)
-        DispatchQueue.main.async {
-            if let temp = self.temperature {
-                annotation.title = "Temperature = \(temp) °C"
-                self.mapView.addAnnotation(annotation)
-            }
-        }
     }
     
-    func addSearchAnnotaion() {
-
-        let annotation = MKPointAnnotation()
-        DispatchQueue.main.async {
-            if let cityName = self.name, let temp = self.temperature, let latitude = self.latitude, let longitude = self.longitude {
-                annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                annotation.title = cityName
-                annotation.subtitle = "Temperature = \(temp) °C"
-                self.mapView.addAnnotation(annotation)
-                
-                let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
-                self.mapView.setRegion(region, animated: true)
-            }
+    func addSearchAnnotaion(name: String, temp: String, latitude: Double, longitude: Double) {
+        
+        DispatchQueue.main.async { [self] in
+            let pin = CustomPin(pinTitle: name, pinSubTitle: "Temperature = \(temp) °C", pinLocation:  CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+            let region = MKCoordinateRegion(center: pin.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
+            
+            self.mapView.setRegion(region, animated: true)
+            self.mapView.addAnnotation(pin)
         }
     }
     
     func getTappedLocation(mapView: MKMapView, gestureRecognizer: UIGestureRecognizer) -> CLLocationCoordinate2D {
-         
+        
         let touchPoint = gestureRecognizer.location(in: mapView)
         return mapView.convert(touchPoint, toCoordinateFrom: mapView)
     }
-    
-
 }
 
 // MARK: - WeatherManagerDelegate
 
 extension WeatherMapViewController: WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-        name = weather.cityName
-        temperature = weather.temperatureString
-        latitude = weather.latitude
-        longitude = weather.longitude
-        
-        addSearchAnnotaion()
+        addSearchAnnotaion(name: weather.cityName, temp: weather.temperatureString, latitude: weather.latitude, longitude: weather.longitude)
     }
     
     func didFailWithError(error: Error) {
@@ -108,27 +96,43 @@ extension WeatherMapViewController: UISearchBarDelegate {
 
 extension WeatherMapViewController {
     func hideKeyboardWhenTappedAround() {
-     let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(WeatherMapViewController.dismissKeyboard))
-      tap.cancelsTouchesInView = false
-      view.addGestureRecognizer(tap)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(WeatherMapViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
     }
     @objc func dismissKeyboard() {
-       view.endEditing(true)
+        view.endEditing(true)
     }
 }
 
 // MARK: - MKMapViewDelegate
 
 extension WeatherMapViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        if view.annotation != nil {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "customAnnotation")
+        annotationView.image = UIImage(named: "location")
+        annotationView.canShowCallout = true
+        
+        let btn = UIButton(type: .detailDisclosure)
+        annotationView.rightCalloutAccessoryView = btn
+        
+        DispatchQueue.main.async { [self] in
+            mapView.selectAnnotation(annotation, animated: true)
+        }
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == annotationView.rightCalloutAccessoryView {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                    guard let detailViewController = storyboard.instantiateViewController(identifier: "WeatherDetail") as? WeatherDetailViewController else { return }
-           // detailViewController.cityName = "Moscow"
-                    
-                    show(detailViewController, sender: nil)
-                
-            }
+            guard let detailViewController = storyboard.instantiateViewController(identifier: "WeatherDetail") as? WeatherDetailViewController else { return }
+            // detailViewController.cityName = "Moscow"
+            show(detailViewController, sender: nil)
         }
     }
-
+}
